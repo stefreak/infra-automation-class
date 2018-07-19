541c0228116dd1f31a68d37eb4ae2238cbcb875f
Renat Nurgaliyev <r.nurgaliyev@syseleven.de>
2018-07-19 23:44:10 +0200

    Install ssh key

--- ci/scripts/global.sh
+++ ci/scripts/global.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-env
-ls -la
+set -e
+
+# Install ssh private key
+if [ -z "SSH_KEY" ]; then
+    exit 1
+else
+    echo $SSH_KEY > /root/.ssh/id_rsa
+    chmod 600 /root/.ssh/id_rsa
+fi
+
+ls -la ~/.ssh/
--- ci/scripts/keystone.sh
+++ ci/scripts/keystone.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-source `dirname $0`/global.sh
+set -e
+
+exec `dirname $0`/global.sh
