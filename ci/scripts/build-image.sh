0f0ae1c5d9ec7b026b940cd3f95dfe5c6e91e77b
Renat Nurgaliyev <r.nurgaliyev@syseleven.de>
2018-08-01 23:32:45 +0200

    test

--- ci/scripts/build-image.sh
+++ ci/scripts/build-image.sh
@@ -5,9 +5,9 @@ set -e
 source `dirname $0`/global.sh
 
 # Extract artifact
-cd release
-tar xzf source.tar.gz --strip 1
-
+# cd release
+# tar xzf source.tar.gz --strip 1
+cd master
 # Run packer
 cd packer
 packer build -debug ./$CI_COMPONENT.json
