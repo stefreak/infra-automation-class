e8f0727454ca92e016b70ba238be12511746a888
Renat Nurgaliyev <r.nurgaliyev@syseleven.de>
2018-08-01 22:38:54 +0200

    Update global.sh

--- ci/scripts/global.sh
+++ ci/scripts/global.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-# Install ssh private key
-if [ -z "SSH_KEY" ]; then
-    exit 1
-else
+# Install ssh private key if set
+if [ ! -z ${SSH_KEY+x} ]; then
     mkdir -p /root/.ssh
     echo "$SSH_KEY" > /root/.ssh/id_rsa
     chmod 600 /root/.ssh/id_rsa
